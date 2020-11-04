30cca7046ef0879b043637d322f601f366b541e0
unknown <ciceroh28@gmail.com>
2020-11-04 16:22:45 -0300

    Instalando tabela usuario

--- install.php
+++ install.php
@@ -5,11 +5,11 @@ banco("DROP TABLE IF EXISTS usuario");
 echo "<p>DB: Tabela usuario apagada...</p>";
 
 $sql  = "CREATE TABLE usuario (";
-$sql .= "codigo SERIAL NOT NULL,";
-$sql .= "nome VARCHAR(250),";
-$sql .= "email VARCHAR (100),";
-$sql .= "senha VARCHAR(100),";
-$sql .= "PRIMARY KAY (codigo),";
+$sql .= "codigo SERIAL NOT NULL, ";
+$sql .= "nome VARCHAR(250), ";
+$sql .= "email VARCHAR (100), ";
+$sql .= "senha VARCHAR(100), ";
+$sql .= "PRIMARY KEY (codigo), ";
 $sql .= ");";
 banco($sql);
 echo "<p>DB: Tabela usuario criada...</p>";
