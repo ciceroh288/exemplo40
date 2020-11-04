74d58165b96aea701ec45fa70fbe282dc91b4a05
unknown <ciceroh28@gmail.com>
2020-11-04 15:53:17 -0300

    install

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 function banco($sql) {
     $conn = pg_connect(getenv("DATABASE_URL"));
     if (!$conn)
-     die("Erro conexão com o banco<br />");
+     die("Erro conexão com o banco.<br />");
 
 
      $resultado = pg_query($conn, $sql);
